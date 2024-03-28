b31c8899a98e6bd5f9cacc0475b0682fb1e9129d
WolnyJunior <soares.junior.dev@gmail.com>
2024-03-28 17:45:51 -0300

    exercicio-2

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require("express")
 
 const app = express();
 
-//PORTA PADRÂO DE ACESSO
+app.get("/sobre", function(req,res){
+    res.send("Olá, tudo bem?\nEssa é minha prmeira app de servidor.")
+})
+
+app.get("/ola/:contato", function(req,res){
+    let contato = req.params.contato
+    res.send("Entre em contato pelo e-mail suporte@email.com")
+})
+
 app.listen(3000, function(){
     console.log("servidor-rodando")
-})    
+})

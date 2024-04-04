2de9a4bcd15f38c547282a7eec167a2fd3bf7bba
WolnyJunior <soares.junior.dev@gmail.com>
2024-04-04 19:21:41 -0300

    exercicio 5

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express")
 
 const app = express();
 
+app.use(express.json())
+
+// Chamar arquivos estáticos através do express
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/public/index.html")
+})
+
 app.get("/sobre", function(req,res){
     res.send("Olá, tudo bem?\nEssa é minha prmeira app de servidor.")
 })
